3aa1c80e79b8405a2e9e51d9773442afe76c8405
instanteach <39467291+instanteach@users.noreply.github.com>
2018-07-10 14:25:32 -0500

    Update The Instanteach Assitant 1.0.py

--- The Instanteach Assitant 1.0.py
+++ The Instanteach Assitant 1.0.py
@@ -1,4 +1,4 @@
-# import os
+import os
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import datetime
@@ -17,7 +17,7 @@ today_date = datetime.date.today()
 def done_checker(response_sheet):
    cell_notdone = response_sheet.find("Not Done")#Finds Row(teacher) Not Yet Done
    row_notdone = cell_notdone.row
-   row_notdone_next=int((row_notdone)+1)
+   row_notdone_next=int((row_notdone)+1)#locate row under
    name_notdone = str(response_sheet.cell(row_notdone,3).value)#teacher name
    timestamp_notdone = response_sheet.cell(row_notdone,1).value
    groupname_notdone =str(response_sheet.cell(row_notdone,4).value)
@@ -27,10 +27,10 @@ def done_checker(response_sheet):
    hoursneeded_notdone=response_sheet.cell(row_notdone,8).value
    email_notdone=str(response_sheet.cell(row_notdone,31).value)
    email_notdone_next=(response_sheet.cell(row_notdone_next,31).value)
-   if email_notdone==email_notdone_next:
+   if email_notdone==email_notdone_next:#checking for multiple entries by the same teacher
      print('watch out! Multiple entry!')
    os.chdir('D:\All Docs\SWMM SCHOL\InstanTeach\Instanteachers\Cohort B')
-   exist_check=os.path.isdir(('D:\All Docs\SWMM SCHOL\InstanTeach\Instanteachers\Cohort B\{}').format(email_notdone))
+   exist_check=os.path.isdir(('D:\All Docs\SWMM SCHOL\InstanTeach\Instanteachers\Cohort B\{}').format(email_notdone))#Check if teacher previously filled out form
    if exist_check==True:
      print('Teacher already exists ;)')
      os.startfile((('D:\All Docs\SWMM SCHOL\InstanTeach\Instanteachers\Cohort B\{}').format(email_notdone)))
@@ -42,9 +42,9 @@ def done_checker(response_sheet):
    print(('{} completed on {}').format(name_notdone,timestamp_notdone))
    print(('level:{}\n number of students:{}\n age group: {}\n lenght of class:{}').format(level_notdone,number_notdone,agegroup_notdone,hoursneeded_notdone))
    answered_cells = (response_sheet.cell(row_notdone,9).value),(response_sheet.cell(row_notdone,10).value),(response_sheet.cell(row_notdone,11).value),(response_sheet.cell(row_notdone,12).value),(response_sheet.cell(row_notdone,13).value),(response_sheet.cell(row_notdone,14).value),(response_sheet.cell(row_notdone,15).value),(response_sheet.cell(row_notdone,16).value),(response_sheet.cell(row_notdone,17).value),(response_sheet.cell(row_notdone,18).value),(response_sheet.cell(row_notdone,19).value),(response_sheet.cell(row_notdone,20).value),(response_sheet.cell(row_notdone,21).value),(response_sheet.cell(row_notdone,22).value),(response_sheet.cell(row_notdone,23).value),(response_sheet.cell(row_notdone,24).value),(response_sheet.cell(row_notdone,25).value),(response_sheet.cell(row_notdone,26).value),(response_sheet.cell(row_notdone,27).value),(response_sheet.cell(row_notdone,28).value),(response_sheet.cell(row_notdone,29).value),(response_sheet.cell(row_notdone,30).value)
-   print(answered_cells)
+   print(answered_cells) #Prints out the info inputted by the teacher
 
-   sendemail_prompt=input('Are you ready to send the email?(y/n)')
+   sendemail_prompt=input('Are you ready to send the email?(y/n)')#Creates email template for new or returning teachers
    if sendemail_prompt=='y' and exist_check==False:
      response_sheet.update_cell(row_notdone,2,'✓')
      response_sheet.update_cell(row_notdone_next,2,'Not Done')
@@ -53,7 +53,7 @@ def done_checker(response_sheet):
       response_sheet.update_cell(row_notdone,2,'✓')
       response_sheet.update_cell(row_notdone_next,2,'Not Done')
       print(('{} Your Classroom Material For "{}" {}-Please Confirm That You Received This Email, Thanks!--\n \n Hello again {} !\n Thanks for filling the form again! I appreciate your trust in us! \n \n Here is some more material based on what you filled out 😁!\n \n Let us know if this material was useful or not for your classes: \n \n And as always, feel free to fill out the questionaire as many times as you might need\n \n English Teaching Material Generator \n \n And have a great time teaching! \n \n Conrad WS').format(email_notdone,groupname_notdone,today_date,name_notdone))
-   repeat_prompt=input('Repeat?(y/n)')
+   repeat_prompt=input('Repeat?(y/n)')#repeats for next entry entry
    if repeat_prompt=='y' or'Y':
      done_checker(response_sheet)
    if repeat_prompt=='n':
@@ -61,6 +61,5 @@ def done_checker(response_sheet):
   
 start_prompt=input('Hi, I m the Instanteach Assistant, here to accelerate the material sending to English teachers around the world.\n  Would you wish to start?(y/n)')
 
-if start_prompt=='y' or 'Y':
+if start_prompt=='y' or 'Y':#Start Prompt
   done_checker(response_sheet)
-
